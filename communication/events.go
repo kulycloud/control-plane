a5dc20533f3f91e4c6ade04df4d672928edaf990
themmj <themmj@users.noreply.github.com>
2021-03-12 20:55:45 +0100

    KU-92 Add event listening logic and proper synchronization

--- communication/events.go
+++ communication/events.go
@@ -1,30 +1,37 @@
 package communication
 
 import (
-	"fmt"
+	commonCommunication "github.com/kulycloud/common/communication"
 
-    protoCommon "github.com/kulycloud/protocol/common"
+	protoCommon "github.com/kulycloud/protocol/common"
 	protoControlPlane "github.com/kulycloud/protocol/control-plane"
 )
 
 type eventStream struct {
-    destination string
-    stream protoControlPlane.ControlPlane_RegisterComponentServer
-    activelyListening map[string]bool
+	destination       string
+	stream            protoControlPlane.ControlPlane_RegisterComponentServer
+	activelyListening map[string]bool
 }
 
 func newEventStream(endpoint *protoCommon.Endpoint, stream protoControlPlane.ControlPlane_RegisterComponentServer) *eventStream {
-    return &eventStream{
-        destination: fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port),
-        stream: stream,
-    }
+	return &eventStream{
+		destination:       commonCommunication.NewIdentifierFromEndpoint(endpoint),
+		stream:            stream,
+		activelyListening: map[string]bool{},
+	}
 }
 
 func (es *eventStream) listenOnEvent(eventType string) {
-    es.activelyListening[eventType] = true
+	es.activelyListening[eventType] = true
 }
 
 func (es *eventStream) send(event *protoCommon.Event) error {
-    return es.stream.Send(event)
+	if es.activelyListening[event.Type] {
+		return es.stream.Send(event)
+	}
+	return nil
 }
 
+func (es *eventStream) sendConfirmation() error {
+	return es.stream.Send(&protoCommon.Event{Type: "confirmation"})
+}
--- communication/listener.go
+++ communication/listener.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"github.com/kulycloud/common/logging"
 	"github.com/kulycloud/control-plane/config"
+	protoCommon "github.com/kulycloud/protocol/common"
 	protoControlPlane "github.com/kulycloud/protocol/control-plane"
-    protoCommon "github.com/kulycloud/protocol/common"
 	"google.golang.org/grpc"
 	"net"
+	"sync"
 )
 
 var _ protoControlPlane.ControlPlaneServer = &Listener{}
@@ -20,13 +21,14 @@ type Listener struct {
 	server   *grpc.Server
 	listener net.Listener
 
-    eventStreams map[string]*eventStream
+	streamMutex  sync.Mutex
+	eventStreams map[string]*eventStream
 }
 
 func NewListener() *Listener {
 	return &Listener{
-        eventStreams: make(map[string]*eventStream, 0),
-    }
+		eventStreams: make(map[string]*eventStream, 0),
+	}
 }
 
 func (listener *Listener) Start() error {
@@ -47,25 +49,51 @@ func (listener *Listener) RegisterComponent(message *protoControlPlane.RegisterC
 	if err != nil {
 		logger.Warnw("error connecting to component", "type", message.Type, "endpoint", message.Endpoint, "error", err)
 	}
-    eventStream := newEventStream(message.Endpoint, stream)
-    listener.eventStreams[eventStream.destination] = eventStream
+
+	// create event stream and store it
+	eventStream := newEventStream(message.Endpoint, stream)
+	logger.Debugf("created new event stream for %s", eventStream.destination)
+	listener.streamMutex.Lock()
+	listener.eventStreams[eventStream.destination] = eventStream
+	listener.streamMutex.Unlock()
+
+	// send confirmation that the stream has been created
+	// no errors indicate a working stream so it waits until the streams context is closed
+	err = eventStream.sendConfirmation()
+	if err == nil {
+		<-stream.Context().Done()
+		logger.Debug("stream context done, deleting stream")
+		listener.streamMutex.Lock()
+		delete(listener.eventStreams, eventStream.destination)
+		listener.streamMutex.Unlock()
+	}
 
 	return err
 }
 
 func (listener *Listener) CreateEvent(ctx context.Context, event *protoCommon.Event) (*protoCommon.Empty, error) {
-    for destination, stream := range listener.eventStreams {
-        err := stream.send(event)
-        if err != nil {
-            logger.Warn("error while sending event to stream %w", err)
-            delete(listener.eventStreams, destination)
-        }
-    }
-    return nil, nil
+	logger.Debugf("creating event %v", event)
+	listener.streamMutex.Lock()
+	defer listener.streamMutex.Unlock()
+	for destination, stream := range listener.eventStreams {
+		err := stream.send(event)
+		if err != nil {
+			logger.Warnf("error while sending event to stream %v, deleting stream", err)
+			delete(listener.eventStreams, destination)
+		}
+		logger.Debugf("sent event to %s", destination)
+	}
+	return &protoCommon.Empty{}, nil
 }
 
 func (listener *Listener) ListenToEvent(ctx context.Context, request *protoControlPlane.ListenToEventRequest) (*protoCommon.Empty, error) {
-    
-    return nil, nil
+	logger.Debugf("%s requested to receive %s events", request.Destination, request.Type)
+	listener.streamMutex.Lock()
+	defer listener.streamMutex.Unlock()
+	stream, ok := listener.eventStreams[request.Destination]
+	if !ok {
+		return &protoCommon.Empty{}, fmt.Errorf("could not find associated stream")
+	}
+	stream.listenOnEvent(request.Type)
+	return &protoCommon.Empty{}, nil
 }
-
